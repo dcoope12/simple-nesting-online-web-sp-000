bf2cf137fcb93f18fb85e420fa5ad665956dd91d
Devin Cooper <dcoope12@asu.edu>
2020-09-17 03:30:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,7 +65,10 @@ def adding_matz
 
 	programmer_hash =
  		{
-
+			:yukihiro_matsumoto => {
+		  :known_for => "Ruby",
+		  :languages => ["LISP", "C"]
+		},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,11 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash << :yukihiro_matsumoto => {
- :known_for => "Ruby",
- :languages => ["LISP", "C"]
- }
- programmer_hash[:yukihiro_matsumoto]
+ 
+ programmer_hash[0]
 end
 
 def changing_alan
